8ef7c296870fcbd2390021c9431754e5dbd2daff
Matt Pahuta <mhpahuta@gmail.com>
2026-01-14 15:43:29 -0800

    Add Vans page, route, and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./routes/Home"
-import About from "./routes/About"
-import "./server"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Vans from "./routes/Vans";
+import "./server";
 
 function App() {
-
   return (
     <BrowserRouter>
       <header className="mx-auto max-w-2xl flex items-center justify-between py-6 px-4">
-        <Link className="font-bold text-2xl" to="/">#vanlife</Link>
+        <Link className="font-bold text-2xl" to="/">
+          #vanlife
+        </Link>
         <nav className="flex gap-3.5 font-medium text-lg">
-          <Link className="hover:underline" to="/about">About</Link>
-          <Link className="hover:underline" to="/vans">Vans</Link>
+          <Link className="hover:underline" to="/about">
+            About
+          </Link>
+          <Link className="hover:underline" to="/vans">
+            Vans
+          </Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
---
+++ src/routes/Vans.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+function Vans() {
+
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then(res => res.json())
+      .then(data => setVans(data.vans))
+  }, []);
+
+  const vanCards = vans.map(van => (
+    <li key={van.id} className="group p-2">
+      <img src={van.imageUrl} alt={van.name} className="w-full aspect-square object-cover rounded-lg" />
+      <div className="pt-4 flex items-center justify-between">
+        <h2 className="text-xl font-semibold">{van.name}</h2>
+        <p className="text-xl font-semibold flex flex-col">${van.price}
+          <span className="block font-normal text-sm self-end">/day</span>
+        </p>
+      </div>
+      <span className={`inline-flex items-center px-4 py-1 rounded-md bg-orange-400 text-white font-semibold`}>{van.type}</span>
+    </li>
+  ))
+
+  return (
+    <main className="mx-auto max-w-2xl py-16 px-4">
+      <h1 className="text-3xl font-bold">Explore our van options</h1>
+      {/* Van filters */}
+      <ul className="flex"></ul>
+      {/* Van card grid */}
+      <ul className="py-10 grid sm:grid-cols-2 gap-y-5 gap-x-2">
+        {vanCards}
+      </ul>
+    </main>
+  );
+}
+
+export default Vans;
