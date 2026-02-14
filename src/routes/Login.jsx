f574773f1dc9f53bcc25be8702fb901f55af66e4
Matt Pahuta <mhpahuta@gmail.com>
2026-02-14 12:44:32 -0800

    Add login form authentication and error handling mock ups

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
+import { loginUser } from "../api";
 
 export default function Login() {
   const [loginFormData, setLoginFormData] = useState({ email: "", password: "" })
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null)
+  const location = useLocation();
 
   function handleSubmit(e) {
     e.preventDefault()
-    console.log(loginFormData)
+    setStatus("submitting");
+    loginUser(loginFormData)
+      .then((data) => {
+        console.log(data)
+        setError(null)
+      }).catch((error) => {
+        setError(error)
+      }).finally(() => {
+        setStatus("idle")
+      })
   }
 
   function handleChange(e) {
@@ -19,7 +32,11 @@ export default function Login() {
 
   return (
     <div className="login-container">
+      {
+        location.state?.message && <p className="text-xl text-red-500 font-semibold">{location.state.message}</p>
+      }
       <h1>Sign in to your account</h1>
+      {error?.message && <p className="text-xl text-red-500 font-semibold">{error.message}</p> }
       <form onSubmit={handleSubmit} className="login-form">
         <input
             name="email"
@@ -35,7 +52,9 @@ export default function Login() {
             placeholder="Password"
             value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
       </form>
     </div>
   )
